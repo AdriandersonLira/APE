e9766e0cae3c51beb5a4415b4d057e401dd5b6b0
Adrianderson Lira <43156311+AdriandersonLira@users.noreply.github.com>
2019-05-03 00:02:44 -0300

    Update ex008.py

--- Lista_de_Exercício_06/ex008.py
+++ Lista_de_Exercício_06/ex008.py
@@ -1,6 +1,6 @@
 massa = float(input('Digite a quantidade de massa: '))
 tempo = 0
-while massa <= 0.5:
+while massa >= 0.5:
     tempo += 50
     massa /= 2
 print(f'{tempo} segundos para a massa se tornar menor que 0,5g')
