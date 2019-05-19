95bdf6693334e65304b2b7b9d35edb90e662c877
Adrianderson Lira <43156311+AdriandersonLira@users.noreply.github.com>
2019-05-19 02:22:04 -0300

    Delete ex002.py

--- Lista_de_Exercícios_07/ex002.py
+++
@@ -1,9 +0,0 @@
-n = int(input('Digite o tamanho do seu vetor: '))
-vetor = list()
-
-for i in range(n):
-    numero = int(input('Digite um número: '))
-    vetor.append(numero)
-
-print(vetor)
-print(vetor[::-1])
